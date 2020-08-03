87c0a1a746573338ee8eb84e9d14e756ddc4de8b
Tom <thomas.spencer@codurance.com>
2020-08-03 14:54:48 +0100

    Add passing test - not quite testing functionality

--- src/test/java/com/codurance/StackShould.java
+++ src/test/java/com/codurance/StackShould.java
@@ -2,20 +2,39 @@ package com.codurance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StackShould {
 
+  private Stack stack;
+  private Object object;
+
+  @BeforeEach
+  void setUp() {
+    stack = new Stack();
+    object = new Object();
+  }
+
   @Test
   void push_into_empty_stack() {
-    // given
-    Stack stack = new Stack();
-    Object object = new Object();
-
     // when
     stack.push(object);
 
     // then
     assertThat(stack.pop()).isSameAs(object);
+  }
+
+  @Test
+  void add_when_stack_is_not_empty() {
+    // given
+    stack.push(object);
+
+    // when
+    Object object2 = new Object();
+    stack.push(object2);
+
+    // then
+    assertThat(stack.pop()).isSameAs(object2);
   }
 }
